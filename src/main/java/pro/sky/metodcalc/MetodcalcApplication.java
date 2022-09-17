8e66bda3c07ca71b7a9c4fda632fbf4fa2fded0c
xmin3 <Xmin.34@gmail.com>
2022-09-17 12:53:05 +0300

    Commit

---
+++ src/main/java/pro/sky/metodcalc/MetodcalcApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.metodcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodcalcApplication.class, args);
+
+	}
+
+}
